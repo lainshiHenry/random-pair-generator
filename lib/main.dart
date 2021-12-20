f1dd8b6a97f5a5a1c86628521c72c15cbc639470
henryle <henryle913@outlook.com>
2021-12-20 01:41:36 -0500

    init commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_pair_generator/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: ,
+      home: const HomeScreen(),
     );
   }
 }
